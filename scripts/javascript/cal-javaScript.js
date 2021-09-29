eaf3f64ec4cf03bcaae4158b63615eb1205ef500
Mateo Mora <23mateom@mydacc.org>
2021-09-29 09:48:19 -0400

    update

--- scripts/javascript/cal-javaScript.js
+++ scripts/javascript/cal-javaScript.js
@@ -1,1 +1,15 @@
-let calorieNumber = 0
+document.getElementById("calorieNumber").addEventListener('click', function() {
+    var text = document.getElementById('calorieNumber');
+    text.value = "";
+});
+/*for the calorie calculator, the goal is to click the picture, and to have an individual function run for each picture that adds the number of calories to a specific text box, preferably you wont be allowed to type in the text box*/
+
+
+function finale() {
+    function fries(){
+        let newNumber = document.getElementById("calorieNumber") + 999;
+    }
+
+
+    let calorieNumber = newNumber;
+}
