1fa7f53d5e36185b14179cd84fea346ef13372b7
Seliskar <23SeliskarJ@mydacc.org>
2021-09-29 10:30:51 -0400

    update

--- scripts/javascript/cal-javaScript.js
+++ scripts/javascript/cal-javaScript.js
@@ -3,13 +3,9 @@ document.getElementById("calorieNumber").addEventListener('click', function() {
     text.value = "";
 });
 /*for the calorie calculator, the goal is to click the picture, and to have an individual function run for each picture that adds the number of calories to a specific text box, preferably you wont be allowed to type in the text box*/
+let newNumber = 0;
 
-
-function finale() {
-    function fries(){
-        let newNumber = document.getElementById("calorieNumber") + 999;
-    }
-
-
-    let calorieNumber = newNumber;
+function fries(){
+    let newNumber = document.getElementById("calorieNumber") + 378;
+    document.getElementById("calorieNumber").innerHTML = newNumber;
 }
