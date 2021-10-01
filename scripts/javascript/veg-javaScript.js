4dc7d2fcc7dc49fd7e0d8a14e12b470c29069792
Bollentino <23BollentinoA@mydacc.org>
2021-10-01 09:57:00 -0400

    hdfgbhbweffwehwefvjhefwvhsfessfdvhjfwevb

--- scripts/javascript/veg-javaScript.js
+++ scripts/javascript/veg-javaScript.js
@@ -3,16 +3,36 @@ let placeHolderTwo = document.querySelector('#place2');
 let placeHolderThree = document.querySelector('#place3');
 let placeHolderFout = document.querySelector('#place4');
 function homeIndex(){
+    document.getElementById(`header`).innerHTML = "You did it!";
     document.getElementById(`info`).innerHTML = "Success";
+    document.getElementById(`fact1`).innerHTML = "Fact";
+    document.getElementById(`fact2`).innerHTML = "Chaos";
+    document.getElementById(`fact3`).innerHTML = "Goose";
+    document.getElementById(`fact5`).innerHTML = "Oppih";
 }
 function fastFresh(){
+    document.getElementById(`header`).innerHTML = "How did you fail?!";
     document.getElementById(`info`).innerHTML = "Failure";
+    document.getElementById(`fact1`).innerHTML = "Hippopotomonstrosesquippedaliophobia";
+    document.getElementById(`fact2`).innerHTML = "Is the fear of long words";
+    document.getElementById(`fact3`).innerHTML = "Aibohphobia";
+    document.getElementById(`fact5`).innerHTML = "The fear of palindromes";
 }
 function calorieCalculator(){
+    document.getElementById(`header`).innerHTML = "Never gonna give you up";
     document.getElementById(`info`).innerHTML = "www.youtube.com";
+    document.getElementById(`fact1`).innerHTML = "McDonalds";
+    document.getElementById(`fact2`).innerHTML = "KFC";
+    document.getElementById(`fact3`).innerHTML = "Wendy's";
+    document.getElementById(`fact5`).innerHTML = "Burger King";
 }
 function veganOption(){
+    document.getElementById(`header`).innerHTML = "I have loads of ideas!"
     document.getElementById(`info`).innerHTML = "I'm out of ideas";
+    document.getElementById(`fact1`).innerHTML = "Seriously help";
+    document.getElementById(`fact2`).innerHTML = "I ran out of funny";
+    document.getElementById(`fact3`).innerHTML = "I need funny!";
+    document.getElementById(`fact5`).innerHTML = "Welp, ciao!";
 }
 placeHolderOne.addEventListener(`click`, homeIndex);
 placeHolderTwo.addEventListener(`click`, fastFresh);
